b6b359696856fbcbf404c4fe5241401915f76ac8
afcomesana <48021565+afcomesana@users.noreply.github.com>
2026-04-28 20:24:23 +0200

    Delete mongo directory

--- mongo/README.md
+++
@@ -1,1 +0,0 @@
-This folder only contains the code used for comparing the performance between using MongoDB and MySQL to store the records' information.
--- mongo/db_mysql.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-
-mysql_conn = mysql.connector.connect(
-    host="localhost",
-    user="dashboard_usu",
-    password="Mt4?FmQsj$nXCp6t",
-    database="netremor_db"
-)
