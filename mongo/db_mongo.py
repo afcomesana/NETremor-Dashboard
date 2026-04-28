cf9a56cd4b1d63bfff96962403b306ad0cc40188
afcomesana <48021565+afcomesana@users.noreply.github.com>
2026-04-28 20:20:59 +0200

    Clean up db_mongo.py by removing comments
    Removed commented-out code for clarity.

--- mongo/db_mongo.py
+++ mongo/db_mongo.py
@@ -3,27 +3,8 @@ from pymongo.mongo_client import MongoClient
 uri      = "mongodb://138.4.22.25:27017"
 client   = MongoClient(uri)
 
-# print(client.server_info())
-
 database = client["sth_netremor"]
 
 ambulatory_collection      = database["sth_ambulatory_data"]
 ambulatory_collection_acc  = database["sth_ambulatory_data_accelerometer"]
 ambulatory_collection_gyro = database["sth_ambulatory_data_giroscope"]
-
-# print(database.command("listCollections"))
-
-
-# databases = client.list_database_names()
-
-# for collection_name in database.list_collection_names():
-#     print(collection_name)
-#     collection_instance = database[collection_name]
-    
-#     for item in collection_instance.find():
-#         print(item)
-#         break
-        
-
-# for record in collection.find():
-#     pprint.pprint(record)
